66082a826888a0a9ba7c3df8ef62a7f7acecba74
Toren Long <venlercode@gmail.com>
2025-10-23 18:53:30 -0500

    Cleaned up UI for home page a bit

--- frontend/tamu-dues/app/components/SignIn.tsx
+++ frontend/tamu-dues/app/components/SignIn.tsx
@@ -1,21 +1,67 @@
+"use client";
+
+import {useState} from "react";
+import { useRouter } from "next/navigation";
+
 export default function Signin() {
+    const router = useRouter();
     const loginURL = "http://localhost:8080/v1/login";
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const res = await fetch(loginURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (!res.ok) {
+                const err = await res.json();
+                setError(err.message || "Login failed");
+                return;
+            }
+
+            const data = await res.json();
+            localStorage.setItem("authToken", data.token);
+
+            router.push("/dashboard");
+        } 
+        catch (err) {
+            console.error(err);
+            setError("Something went wrong. Please try again.");
+        }
+  };
+
     return (
         <div className="signInForm">
-            <form className="flex flex-col text-center" action={loginURL} method="POST">
+            <form className="flex flex-col text-center" onSubmit={handleSubmit}>
                 <h2>Sign In</h2>
 
-                <label htmlFor="username">Username</label>
-                <input type="username" id="username" name="username" placeholder="Enter your username" required />
+                <input id="username"
+                    placeholder="Username"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required />
 
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                <input id="password"
+                    placeholder="Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required />
 
                 <button type="submit">Sign In</button>
-
-                <div className="extra-links">
-                    <a href="#">Create an account</a>
-                </div>
+                {error && <p>{error}</p>}
             </form>
         </div>
     )
