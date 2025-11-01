da1bd6c9c9a25627e4b3e94140421afe6df7056b
Toren Long <venlercode@gmail.com>
2025-11-01 11:18:34 -0500

    Fixed dueDate time display, with some minor bug fixes

--- frontend/tamu-dues/components/AssignmentPopup.tsx
+++ frontend/tamu-dues/components/AssignmentPopup.tsx
@@ -18,13 +18,32 @@ export default function AssignmentPopup({
 }: PopupProps) {
   if (!assignment) return null;
 
+  const customizeDate = (dateStr: string) => {
+    const date = new Date(dateStr)
+    const options: Intl.DateTimeFormatOptions = { 
+      year: "numeric", 
+      month: "long", 
+      day: "numeric", 
+      hour: "2-digit", 
+      minute: "2-digit",
+      timeZoneName: "short"
+    };
+
+    // Format and replace text content
+    return date.toLocaleString("en-US", options);
+  }
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="bg-green-500 text-white p-6 rounded-lg w-96 relative">
         <button onClick={onClose} className="absolute top-3 right-3 font-bold">✕</button>
         <h2 className="text-xl font-semibold">{assignment.name}</h2>
-        <p>Due: {assignment.dueDate}</p>
+        <p>Due: {customizeDate(assignment.dueDate)}</p>
         <p>Status: {assignment.status}</p>
+        <p>Priority: {assignment.priority}</p>
+
+        <hr className="my-2"></hr>
+
         <p>{assignment.description}</p>
 
         <div className="flex justify-between mt-4">
